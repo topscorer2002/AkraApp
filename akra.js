295eb23c318a6ef642fabdb68e2d91e70e11b7b1
topscorer2002 <falko.carl@gmx.de>
2016-11-29 15:26:52 +0100

    Veriosn 1.0.1
    -Can connect to our real Database instead of localhost
    - dont to type in IDs no more for DB Posts per UI (Autoinkrement is
    working)
    - Add and Get Methods for
    JobStatus, ApplicantLevel and Skills added in Backend and in Frontend
    (for tests)

--- akra.js
+++ akra.js
@@ -11,14 +11,15 @@ var connection = mysql.createPool({
 
 	connectionLimit: 50,
 	////properties...
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'schmal'
-	//host: '213.238.59.191'
-	//user: 'studi'
-	//password: 'jeiGh+o6'
+	//host: 'localhost',
+	//user: 'root',
+	//password: '',
 	//database: 'schmal'
+	host: '213.238.59.191',
+	user: 'studi',
+	password: 'jeiGh+o6',
+	database: 'schmal',
+	port: '3306'
 
 });
 
@@ -50,17 +51,6 @@ app.get('/login', function(req, resp) {
 	resp.sendFile('./files/login.html', {root: __dirname});	
 });
 
-app.post('/login', function(req, resp) {
-	//resp.end(JSON.stringify(req.body));
-	session = req.session;
-	if(session.uniqueID) {
-		resp.redirect('redirects');
-	}
-	if(req.body.username == 'admin' && req.body.password == 'admin') { //hier passwortabfrage - später Vgl. aus Datenbank
-		session.uniqueID = req.body.username;  //NutzerName aus Form wird in session.uniqueID geschrieben
-	}
-	resp.redirect('/redirects');
-});
 
 app.get('/logout', function(req, resp) {
 	req.session.destroy();
@@ -70,11 +60,9 @@ app.get('/logout', function(req, resp) {
 app.get('/index', function (req, resp) {
 	session = req.session;
 	if(session.uniqueID != 'admin') {
-		
 		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
 	
 	} else {
-
 		//resp.send('Du bist angemeldet als Admin. <a href="/logout">Logout</a');
 		//resp.sendFile('index.html', {root: path.join(__dirname, './files')});
 		resp.render('index');
@@ -85,11 +73,9 @@ app.get('/index', function (req, resp) {
 app.get('/addApplicant', function (req, resp) {
 	session = req.session;
 	if(session.uniqueID != 'admin') {
-		
 		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
 	
 	} else {
-
 		resp.sendFile('addapplicant.html', {root: path.join(__dirname, './files')});
 		
 		}
@@ -98,24 +84,60 @@ app.get('/addApplicant', function (req, resp) {
 app.get('/addJob', function (req, resp) {
 	session = req.session;
 	if(session.uniqueID != 'admin') {
-		
 		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
 	
 	} else {
-		
 		resp.sendFile('addJob.html', {root: path.join(__dirname, './files')});
 		
 		}
 });
 
-app.get('/getJobs', function (req, resp) {
+
+app.get('/addSkill', function (req, resp) {
 	session = req.session;
 	if(session.uniqueID != 'admin') {
 		
 		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
 	
 	} else {
+		resp.sendFile('addSkill.html', {root: path.join(__dirname, './files')});
 		
+		}
+});
+
+app.get('/addJobStatus', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	
+	} else {
+		resp.sendFile('addJobStatus.html', {root: path.join(__dirname, './files')});
+		
+		}
+});
+
+
+app.get('/addApplicantLevel', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	
+	} else {
+		resp.sendFile('addApplicantLevel.html', {root: path.join(__dirname, './files')});
+		
+		}
+});
+
+///////////Datenbank-Listen ausgeben
+//Jobs abholen
+app.get('/getJobs', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	
+	} else {
 			//bestimmten Datensatz ausgeben
 			//var id = '1; drop table job;'; //bestimmen Datensatz ausgeben
 			//var query = connection.query('select * from job where job_id = ?', id, function(err, result) {
@@ -124,47 +146,98 @@ app.get('/getJobs', function (req, resp) {
 			//alle Datensätze ausgeben
 				var query = connection.query('select * from job' , function(error, rows, result) {
 					//connection.release();
-				if (!!error) {
+					if (!!error) {
 
-					console.log('Error in the query');
-
-				} else {
-					
-					resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
+						console.log('Error in the query');
+					} else {		
+						resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
 					//console.log(result);
-				}
-  					
+					}	
 				});
 			}
 });
 
 
+//Applicants abholen
 app.get('/getApplicants', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	} else {
+			//alle Datensätze ausgeben
+				var query = connection.query('select * from applicant' , function(error, rows, result) {
+
+					if (!!error) {
+						console.log('Error in the query');
+					} else {	
+						resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
+					//console.log(result);
+					}	
+				});
+			}
+});
+
+///Skills ausgeben
+app.get('/getSkills', function (req, resp) {
 	session = req.session;
 	if(session.uniqueID != 'admin') {
 		
 		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
-	
 	} else {
-		
 			//alle Datensätze ausgeben
-				var query = connection.query('select * from applicant' , function(error, rows, result) {
+				var query = connection.query('select * from skill' , function(error, rows, result) {
 
-				if (!!error) {
-
-					console.log('Error in the query');
-
-				} else {
-					
-					resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
+					if (!!error) {
+						console.log('Error in the query');
+					} else {
+						resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
 					//console.log(result);
-				}
-  					
+					}	
 				});
 			}
 });
 
+//alle job status ausgeben
+app.get('/getJobStatus', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	} else {
+			//alle Datensätze ausgeben
+				var query = connection.query('select * from job_status' , function(error, rows, result) {
 
+					if (!!error) {
+						console.log('Error in the query');
+					} else {
+						resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
+					//console.log(result);
+					}	
+				});
+			}
+});
+
+//alle applicant level ausgeben
+app.get('/getApplicantLevels', function (req, resp) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		
+		resp.send('Unauthorisierter Zugriff <a href="/login">Zurück zum Login</a');
+	} else {
+			//alle Datensätze ausgeben
+				var query = connection.query('select * from applicant_level' , function(error, rows, result) {
+
+					if (!!error) {
+						console.log('Error in the query');
+					} else {
+						resp.json(rows); //zeile wird als json anstatt als objekt (resp.send(rows)) an Browser gesendet
+					//console.log(result);
+					}	
+				});
+			}
+});
+
+////////////////Standard Redirect Page
 app.get('/redirects', function(req, resp) {
 	session = req.session;
 	if(session.uniqueID == 'admin') {
@@ -177,11 +250,28 @@ app.get('/redirects', function(req, resp) {
 });
 
 
+//////////////////////POST METHODS//////////////////////
+
+//LOGIN
+app.post('/login', function(req, resp) {
+	//resp.end(JSON.stringify(req.body));
+	session = req.session;
+	if(session.uniqueID) {
+		resp.redirect('redirects');
+	}
+	if(req.body.username == 'admin' && req.body.password == 'admin') { //hier passwortabfrage - später Vgl. aus Datenbank
+		session.uniqueID = req.body.username;  //NutzerName aus Form wird in session.uniqueID geschrieben
+	}
+	resp.redirect('/redirects');
+});
+
+
+//add applicant
 app.post('/addapplicant', function(req, resp) {
 	//resp.end(JSON.stringify(req.body));
 
 	var applicant = {
-  		applicant_id: req.body.formApplicant_id,
+  		//applicant_id: req.body.formApplicant_id,
   		prename: req.body.formPrename,
   		surname: req.body.formSurname,
   		phone: req.body.formPhone,
@@ -202,14 +292,16 @@ app.post('/addapplicant', function(req, resp) {
 
 });
 
+
+//add Job
 app.post('/addJob', function(req, resp) {
 	//resp.end(JSON.stringify(req.body));
 
 	var job = {
-  		job_id: req.body.formJob_id,
+  		//job_id: req.body.formJob_id,
   		title: req.body.formTitle,
   		job_status_id: req.body.formJob_status_ID,
-  		skill_id: req.body.formSkill_ID,
+  		skill_id: req.body.formSkill_ID
 		};
 
 	var query = connection.query('insert into job set ?', job, function (err, result) {
@@ -221,6 +313,60 @@ app.post('/addJob', function(req, resp) {
 		});
 
 });
+
+
+//add skill
+app.post('/addSkill', function(req, resp) {
+	//resp.end(JSON.stringify(req.body));
+	var skill = {
+  		//skill_id: req.body.fromSkill_id,
+  		skill: req.body.formSkill
+		};
+
+	var query = connection.query('insert into skill set ?', skill, function (err, result) {
+  		if (err) {
+    		console.error(err);
+    		return;
+  			}
+  			console.error(result);
+		});
+});
+
+//add Job Status
+app.post('/addJobStatus', function(req, resp) {
+	//resp.end(JSON.stringify(req.body));
+	var job_status = {
+  		//job_status_id: req.body.formJob_status_ID,
+  		job_status: req.body.formJob_status
+		};
+
+	var query = connection.query('insert into job_status set ?', job_status, function (err, result) {
+  		if (err) {
+    		console.error(err);
+    		return;
+  			}
+  			console.error(result);
+		});
+});
+
+//add Applicant Level
+app.post('/addApplicantLevel', function(req, resp) {
+	//resp.end(JSON.stringify(req.body));
+	var applicant_level = {
+  		//applicant_level_id: req.body.formApplicant_level_ID,
+  		applicant_level: req.body.formApplicant_level
+		};
+
+	var query = connection.query('insert into applicant_level set ?', applicant_level, function (err, result) {
+  		if (err) {
+    		console.error(err);
+    		return;
+  			}
+  			console.error(result);
+		});
+});
+
+
 
 app.listen(1337, function() {
 	console.log('Listening at Port 1337');
